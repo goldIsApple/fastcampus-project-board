22d8cde67c901560fe5bf29046b71a601625101b
goldIsApple <kes98202@naver.com>
2023-09-11 08:29:20 +0900

    1 도메인 클래스 == 1 테이블 매칭되도록 설계했다. 생성자와 `equals()`.`hashcode()` 등이 도메인 맥락에 맞게끔 디테일하게 설정함

--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -1,16 +1,54 @@
 package com.fastcampus.projectboard.domain;
 
-import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.ToString.Exclude;
 
-public class ArticleComment {
+@Entity
+@Getter
+@ToString
+@Table( indexes = {
+	@Index(columnList = "content"),
+	@Index(columnList = "createdAt"),
+	@Index(columnList = "createdBy")
+})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = "id")
+public class ArticleComment extends BaseEntity{
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+
+	@Setter
+	@ManyToOne(optional = false,fetch = FetchType.LAZY )
+	@Exclude
 	private Article article; // 게시글 (ID)
+
+	@Setter
+	@Column(nullable = false,length = 500)
 	private String content; // 본문
 
-	private LocalDateTime createdAt; // 생성일시
-	private String createdBy; // 생성자
-	private LocalDateTime modifiedAt; // 수정일시
-	private String modifiedBy; // 수정자
 
+	private ArticleComment(Article article, String content) {
+		this.article = article;
+		this.content = content;
+	}
+	private static ArticleComment of (Article article, String content) {
+		return new ArticleComment(article, content);
+	}
 }
