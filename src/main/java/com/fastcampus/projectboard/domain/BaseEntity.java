c13fcbcd8367de971ea784d6df1360aed110fadc
goldIsApple <kes98202@naver.com>
2023-09-17 15:04:05 +0900

    #13 - BaseEntity 추출
    생성자,생성일시,수정자,수정일시는 엔티티클래스에
    들어가는 요소이고
    도메인과 직접 연관이 없는 요소이므로
    추출이 가능함
    
    # 본문은 위에 작성
    ######## 본문은 한 줄에 최대 72 글자까지만 입력 ########################### -> |
    
    # 꼬릿말은 아래에 작성: ex) #이슈 번호
    
    # --- COMMIT END ---
    # <타입> 리스트
    #   feat    : 기능 (새로운 기능)
    #   fix     : 버그 (버그 수정)
    #   refactor: 리팩토링
    #   style   : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs    : 문서 (문서 추가, 수정, 삭제)
    #   test    : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore   : 기타 변경사항 (빌드 스크립트 수정 등)
    # ------------------
    #     제목 첫 글자를 대문자로
    #     제목은 명령문으로
    #     제목 끝에 마침표(.) 금지
    #     제목과 본문을 한 줄 띄워 분리하기
    #     본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #     본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # ------------------

--- src/main/java/com/fastcampus/projectboard/domain/BaseEntity.java
+++ src/main/java/com/fastcampus/projectboard/domain/BaseEntity.java
@@ -7,28 +7,33 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 
 @Getter
-@Setter
+@ToString
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
+	@DateTimeFormat(iso = ISO.DATE_TIME)
 	@CreatedDate
-	@Column(nullable = false)
+	@Column(nullable = false,updatable = false)
 	private LocalDateTime createdAt; // 생성일시
 
 	@CreatedBy
-	@Column(nullable = false, length = 100)
+	@Column(nullable = false, length = 100,updatable = false)
 	private String createdBy; // 생성자
 
 	@LastModifiedDate
+	@DateTimeFormat(iso = ISO.DATE_TIME)
 	@Column(nullable = false)
 	private LocalDateTime modifiedAt; // 수정일시
 
